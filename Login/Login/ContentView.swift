a1459431d74425e0d0c145264adc09e56e9f48ef
GHeeJeon <jhjeon_99@naver.com>
2021-04-27 14:50:33 +0900

    solve 5 issue of 7

--- Login/Login/ContentView.swift
+++ Login/Login/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
 
-let storedUsername = "guest"
+let skyBlueColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+let storedUsername = "1234"
 let storedPassword = "1234"
 
 struct ContentView : View {
@@ -30,16 +32,30 @@ struct ContentView : View {
                 logo()
                 UsernameTextField(username: $username, editingMode: $editingMode)
                 PasswordSecureField(password: $password)
+                
                 if authenticationDidFail {
-                    Text("Information not correct. Try again.")
-                        .offset(y: -10)
-                        .foregroundColor(.red)
+                    if self.username == "" {
+                        Text("아이디를 입력해주세요.")
+                            .offset(y: -10)
+                            .foregroundColor(.red)
+                    }
+                    else if self.password == "" {
+                        Text("비밀번호를 입력해주세요.")
+                            .offset(y: -10)
+                            .foregroundColor(.red)
+                    }
+                    else {
+                        Text("아이디와 비밀번호를 다시 확인해주세요.")
+                            .offset(y: -10)
+                            .foregroundColor(.red)
+                    }
                 }
                 Button(action: {
                     if self.username == storedUsername && self.password == storedPassword {
                         self.authenticationDidSucceed = true
                         self.authenticationDidFail = false
-                    } else {
+                    }
+                    else {
                         self.authenticationDidFail = true
                     }
                 })
@@ -50,18 +66,18 @@ struct ContentView : View {
                 }
                 .padding()
             if authenticationDidSucceed {
-                Text("Login succeeded!")
+                Text("환영합니다!")
                     .font(.headline)
-                    .frame(width: 250, height: 80)
+                    .frame(width: 250, height: 50)
                     .background(Color.green)
                     .cornerRadius(20.0)
                     .foregroundColor(.white)
                     //change
                     .animation(Animation.default)
             }
-        }
-            .offset(y: editingMode ? -150 : 0)
+        }.offset(y: editingMode ? -10 : 0)
     }
+    
 }
 
 #if DEBUG
@@ -87,10 +103,10 @@ struct logo : View {
     var body: some View {
         return Image("logo")
             .resizable()
-            .frame(width: 175, height: 125)
+            .scaledToFit()
+            .frame(width: 150, height: 100)
             .clipped()
-
-            .padding(.bottom, 30)
+            .padding()
     }
 }
 
@@ -98,11 +114,12 @@ struct symbol : View {
     var body: some View {
         return Image("symbol")
             .resizable()
+            .scaledToFit()
             .aspectRatio(contentMode: .fill)
-            .frame(width: 150, height: 150)
+            .frame(width: 100, height: 100)
             .clipped()
-            .cornerRadius(150)
-            .padding(.top, 30)
+            .padding(.top, 80)
+        
     }
 }
 
@@ -113,9 +130,8 @@ struct LoginButtonContent : View {
             //change
             .foregroundColor(.white)
             .padding()
-            .frame(width: 220, height: 60)
-            .background(Color.blue
-                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/))
+            .frame(width: 350, height: 40)
+            .background(Color(red: 0.323, green: 0.795, blue: 0.988))
             .cornerRadius(15.0)
     }
 }
@@ -127,17 +143,16 @@ struct UsernameTextField : View {
     @Binding var editingMode: Bool
     
     var body: some View {
-        return TextField("Username", text: $username, onEditingChanged: {edit in
+        return TextField("", text: $username, onEditingChanged: {edit in
             if edit == true
             {self.editingMode = true}
             else
             {self.editingMode = false}
         })
+    
             .padding()
-            .overlay(Rectangle().stroke(Color.gray, lineWidth: 1))
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
-            
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+
     }
 }
 
@@ -146,10 +161,9 @@ struct PasswordSecureField : View {
     @Binding var password: String
     
     var body: some View {
-        return SecureField("Password", text: $password)
+        return SecureField("", text: $password)
             .padding()
-            .overlay(Rectangle().stroke(Color.gray, lineWidth: 1))
-            .cornerRadius(5.0)
-            .padding(.bottom, 20)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.top, -20)
     }
 }
